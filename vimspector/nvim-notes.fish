a5d3d80c3b8c6507f259974028d1863e96a8dc28
Wes Higbee <wes.mcclure@gmail.com>
2024-11-20 02:00:34 -0600

    first working config for nvim, cloned repo neovim/neovim and make - built - then ran and debugged it and it just worked OOB cool vimspector rocks my world, I love the debug tab design that is perfect, and I wanna have special keys mapped for that tab! efficiently and liikely diff ie F9/F10/etc

---
+++ vimspector/nvim-notes.fish
@@ -0,0 +1,18 @@
+git clone https://github.com/neovim/neovim
+cd neovim
+make
+
+# THEN add .vimspector.json to repo root
+
+nvim # start debugging instance (probably not the built version!)
+
+:call vimspector#Launch()
+:call vimspector#StepOver()
+:call vimspector#StepInto()
+# etc
+
+# move cursor to a variable (in func with breakpoint)
+# then :call vimspector#InspectVariable() - or <leader>di in my current config
+#   IIUC graphical vim might have mouse support to hover and show this, that would rock in the terminal :(
+
+# profit
